9f920ed0538dc6158b5d566f44f251dc01c336d6
Vlad Firoiu <vladfi2@gmail.com>
2014-06-01 20:44:02 -0400

    2124

--- inf_hypers_noslice.py
+++ inf_hypers_noslice.py
@@ -1,9 +1,9 @@
 from train_birds import *
-model = makeModel(D=6,learnHypers=True,hyperPrior='(normal 0 10)')
-out = getMoves(model,slice_hypers=False,transitions=200,iterations=6,
-               label='prior10_7day_noslice_')
+model = makeModel(D=6,learnHypers=True,hyperPrior='(normal 0 30)')
+out = getMoves(model,slice_hypers=False,transitions=200,iterations=8,
+               label='prior30_7day_noslice_')
 
 print 'NEW JOB, prior normal 0 20 with slice hypers'
-model = makeModel(D=6,learnHypers=True,hyperPrior='(normal 0 20)')
-out = getMoves(model,slice_hypers=False,transitions=200,iterations=6,
-               label='prior20_7day_noslice_')
+model = makeModel(D=6,learnHypers=True,hyperPrior='(normal 0 30)')
+out = getMoves(model,slice_hypers=False,transitions=200,iterations=8,
+               label='prior30_7day_noslice_')
