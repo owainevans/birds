1da60dbcbd0e5c2a8a36bd349bfe63bedb123b61
Vlad Firoiu <vladfi2@gmail.com>
2014-06-03 00:26:51 -0400

    long_gamma

--- inf_hypers_noslice.py
+++ inf_hypers_noslice.py
@@ -1,13 +1,10 @@
 from train_birds import *
-priors=['(gamma .25 .05)']
-labels=['g025_005']
+priors=['(gamma .5 .05)','(gamma .5 .05)','(gamma .5 .05)']
+labels=['g05_005']
 for prior,label in zip(priors,labels):
     model = makeModel(dataset=3, D=3, learnHypers=True, hyperPrior=prior)
     
-    out = getMoves(model,slice_hypers=False,transitions=200,iterations=40,
-               label='gamma_prior_ds3/%s/'%label )
+    out = getMoves(model,slice_hypers=False,transitions=100,iterations=100,
+               label='ds3_long_gamma_prior/%s/'%label )
 
-# print 'NEW JOB, prior normal 0 20 with slice hypers'
-# model = makeModel(D=6,learnHypers=True,hyperPrior='(normal 0 30)')
-# out = getMoves(model,slice_hypers=False,transitions=200,iterations=8,
-#                label='prior30_7day_noslice_')
+
